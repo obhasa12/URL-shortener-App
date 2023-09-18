6ece5625c4e298297116e29e5eba0b9087649b2a
Acen <obhasaputra@gmail.com>
2023-09-19 05:28:50 +0700

    design the react app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Form form "./components/Form"
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={Form} />
+              <Route path='/app' component={Form} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { initializeApp } from 'firebase/app'
+
+import App from './App.jsx'
+import './index.css'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWw9s7iGu7UZSPKOZpH0nGCslBjGjzPq8",
+  authDomain: "url-shortener-app-6565b.firebaseapp.com",
+  projectId: "url-shortener-app-6565b",
+  storageBucket: "url-shortener-app-6565b.appspot.com",
+  messagingSenderId: "886977809806",
+  appId: "1:886977809806:web:305c8576f0331e2cb028df",
+  measurementId: "G-WSPTTD7X5X"
+};
+
+initializeApp(firebaseConfig);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
